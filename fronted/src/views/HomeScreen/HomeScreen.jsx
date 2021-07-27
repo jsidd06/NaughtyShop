3912c9e0e956208557c347525fbcfef2e2d03e66
Siddharth <jsidd06@gmail.com>
2021-07-27 14:31:12 +0530

    in this i have just added style.css LoadingBox MessageBox HomeScreen ProductScreen

--- fronted/src/views/HomeScreen/HomeScreen.jsx
+++ fronted/src/views/HomeScreen/HomeScreen.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import data from '../Data/Data'
-import ProductScreen from '../ProductScreen/ProductScreen'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductScreen from "../ProductScreen/ProductScreen";
+import LoadingBox from "../LoadingBox/LoadingBox";
+import MessageBox from "../MessageBox/MessageBox";
 function HomeScreen() {
-    return (
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
         <div className="row center">
-          {data.products.map((product) => (
+          {products.map((product) => (
             <ProductScreen key={product._id} product={product}></ProductScreen>
           ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;

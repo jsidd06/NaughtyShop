aecbc1cec52510c561dda0910ea1eead395c28e7
Siddharth <jsidd06@gmail.com>
2021-08-02 16:48:14 +0530

    add env package or jwt package beside it and new file .env utils.js or some changes in user router or server.js final it some changes is gitignore

--- Backend/Router/userRouter.js
+++ Backend/Router/userRouter.js
@@ -1,14 +1,39 @@
-import express from 'express'
-import data from '../Data/data.js';
-import User from '../Model/Model.js';
-import expressAsyncHandler from 'express-async-handler';
+import express from "express";
+import data from "../Data/data.js";
+import User from "../Model/Model.js";
+import bcrypt from "bcryptjs";
+import expressAsyncHandler from "express-async-handler";
+import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
-userRouter.get('/seed', expressAsyncHandler ( async (req, res) => {
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
     await User.remove({});
-    const createdUsers = await User.insertMany(data.users)
-    res.send({createdUsers})
-}))
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
 
-export default userRouter
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+      } 
+    } else {
+      res.status(401).send({ message: "Invalid email or password" });
+    }
+  })
+);
+
+export default userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
+dotenv.config();   
 import express from "express";
+import dotenv from "dotenv";
 import userRouter from "./Router/userRouter.js";
 import mongoose from "mongoose";
 import productRouter from "./Router/ProductRouter/ProductRouter.js";
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/naughty", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
